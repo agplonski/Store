a75dc86ae66e6da13040a1f9e205d25840115fdd
Andrew Plonski <agplonski@hotmail.com>
2012-11-24 22:32:29 -0600

    using enviroment production

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Store::Application.initialize!
 
-ActiveRecord::Base.include_root_in_json = true
+ActiveRecord::Base.include_root_in_json = true    
+ENV['RAILS_ENV'] ||= 'production'
